6d8b1d6b24c8a8654f52f543f2be5dfa085d2b40
MohanSrinivas009 <mohansrinivas0802@gmail.com>
2026-04-10 15:28:36 +0530

    app.ja

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get('/orders', (req, res) => {
     });
 
 });
-app.get('/products', (req, res) => {
+app.get('/Products', (req, res) => {
     const query = 'SELECT * FROM Products';
 
     pool.query(query, (err, results) => {
