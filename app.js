c82810d9f4e043cb5e4e53f51431b92b77aa9257
MohanSrinivas009 <mohansrinivas0802@gmail.com>
2026-04-21 10:45:16 +0530

    Product API

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ import mysql from 'mysql2'
 import dotenv from 'dotenv'
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST ,
-  user: process.env.DB_USER ,
-  password: process.env.DB_PASSWORD ,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
+ 
 });
 
 pool.query((err) => {
